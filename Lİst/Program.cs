c59781d587351cb4e454529f3a96e63103f87356
rohat güneş <rohatgns44@gmail.com>
2024-08-20 16:38:41 +1000

    Add project files.

---
+++ Lİst/Program.cs
@@ -0,0 +1,15 @@
+﻿public class program
+{
+    public static void Main(string[] args)
+    {
+
+        List<string> invitess = new List<string>() { "1-bulent ersoy","2-ajda pekkan","3-ebru gundes",
+        "4-hadise","5-hande yener","6-tarkan","7-funda arar","8-demet akalın"};
+
+        foreach (string i in invitess)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+}
